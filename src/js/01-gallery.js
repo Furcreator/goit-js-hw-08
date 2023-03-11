632ac920c68bd0866e50f62f987adb8bcdc4d963
Daniil Mikhnevich <dan.mikhnevich20@gmail.com>
2023-03-11 22:39:23 +0200

    task1 and task2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,52 +1,38 @@
 
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-import { galleryItems } from './gallery-items';
 
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const gallery = document.querySelector(".gallery");
 const galleryMarkup = createGalleryItems(galleryItems);
-gallery.insertAdjacentHTML("beforeend", galleryMarkup);
-gallery.addEventListener("click", onClickGalleryItem);
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+gallery.addEventListener('click', onClickGalleryItems);
 
 function createGalleryItems() {
   return galleryItems
     .map(({ preview, description, original }) => {
-      return `<div class="gallery__item">
-        <a class="gallery__link" href="${original}">
-        <img
-            class="gallery__image"
-            src="${preview}"
-            data-source="${original}"
-            alt="${description}"
-          />
+      return `<li>
+        <a class="gallery__item" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
         </a>
-      </div>`;
+      </li> `;
     })
     .join("");
 }
 
-function onClickGalleryItem(evt) {
-  blockAction(evt);
-  onOpenAndCloseGalleryItem(evt);
-}
-
-function blockAction(evt){
-  evt.preventDefault();
-}
-function onOpenAndCloseGalleryItem(evt){
-  if(evt.target.nodeName !== 'IMG'){
-    return;
-  }
-  const instance = basicLightbox.create(`
-  <img width="1400" height="900" src="${evt.target.dataset.source}">`);
-  instance.show();
-  
-  gallery.addEventListener('keydown', onEscCloseGalleryItem)
-  function onEscCloseGalleryItem(evt){
-    if(evt.code === 'Escape'){
-      instance.close();
-      gallery.removeEventListener('keydown', a)
+function onClickGalleryItems(e){
+    blockStandartAction(e);
+    if(e.target.nodeName !== 'IMG'){
+        return;
     }
-  }
+}
+var lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
+
+function blockStandartAction(e){
+    e.preventDefault()
 }
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+player.on('timeupdate', throttle(data=>{
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data)
+    )}, 1000));
+const currentTimeBeforeReload = JSON.parse(localStorage.getItem('videoplayer-current-time'))
+player.setCurrentTime(currentTimeBeforeReload.seconds)
