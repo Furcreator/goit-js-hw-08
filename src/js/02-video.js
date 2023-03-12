7e1f89b0ad763e3f0638f259feae789f789e4aeb
Daniil Mikhnevich <dan.mikhnevich20@gmail.com>
2023-03-12 04:09:17 +0200

    tasks complete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,16 @@
-import Player from "@vimeo/player";
-import throttle from "lodash.throttle";
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-
-player.on('timeupdate', throttle(data=>{
-    localStorage.setItem('videoplayer-current-time', JSON.stringify(data)
-    )}, 1000));
-const currentTimeBeforeReload = JSON.parse(localStorage.getItem('videoplayer-current-time'))
-player.setCurrentTime(currentTimeBeforeReload.seconds)
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+  }, 1000)
+);
+const currentTimeBeforeReload = JSON.parse(
+  localStorage.getItem('videoplayer-current-time')
+);
+player.setCurrentTime(currentTimeBeforeReload.seconds);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
 
+import throttle from "lodash.throttle";
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(addToLocalStorage, 500))
+form.addEventListener('submit', onSubmit)
+
+localStorageInput();
+
+function addToLocalStorage(e){
+    formData[e.target.name] = e.target.value;
+    const JsonFormData = JSON.stringify(formData);
+    localStorage.setItem(STORAGE_KEY, JsonFormData);
+}
+
+function onSubmit(e){
+    e.preventDefault();
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY)
+}
+
+function localStorageInput(){
+    const setMassage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if(setMassage){
+        Object.entries(setMassage).forEach(([key, value]) => {
+            form.elements[key].value = value;
+        });
+        
+    }
+}
